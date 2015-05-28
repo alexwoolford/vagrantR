ecbc5c88c7527fc82d48bdb20c91ab66ce9a3211
Alex Woolford <awoolford@altitudedigital.com>
2015-05-28 08:10:45 -0600

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,20 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+
+Vagrant.configure(2) do |config|
+
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.synced_folder "shared/", "/shared"
+
+  config.vm.provision "puppet" do |puppet|
+    puppet.module_path = "modules"
+  end
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = true
+    vb.memory = "1024"
+  end
+
+end
---
+++ manifests/default.pp
@@ -0,0 +1,52 @@
+
+include apt
+
+apt::key { 'cran':
+  id      => 'E084DAB9',
+  server  => 'keyserver.ubuntu.com',
+}
+
+apt::source { 'R':
+  comment  => 'This is the apt repository for R - the language for statistical computing',
+  location => 'http://cran.rstudio.com/bin/linux/ubuntu/',
+  release  => 'trusty/',
+  repos    => '',
+}
+
+exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+}
+
+Exec["apt-update"] -> Package <| |>
+
+package { "r-base":
+  ensure  => latest,
+  require  => Exec['apt-update'],
+}
+
+package { "littler":
+  ensure  => latest,
+}
+
+
+exec {
+    command => "r -e \"install.packages('forecast', repos='http://cran.rstudio.com')\""
+}
+
+
+
+/*
+
+
+
+
+#exec { "apt-update":
+#    command => "r -e \"install.packages('forecast', repos='http://cran.rstudio.com')\""
+#}
+
+#r -e "install.packages('forecast', repos='http://cran.rstudio.com')"
+#r -e "install.packages('RMySQL', repos='http://cran.rstudio.com')"
+#r -e "install.packages('dplyr', repos='http://cran.rstudio.com')"
+#r -e "install.packages('zoo', repos='http://cran.rstudio.com')"
+
+*/
